b16d49d6d97befd7b4b378b9db9edb9c30b3130d
Alex Archer <aga7@pitt.edu>
2015-09-27 21:59:11 -0400

    added Lab3a.java

---
+++ Lab3a.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab3a;
+
+/**
+ *
+ * @author Alex
+ */
+public class Lab3a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        double distanceApart = 10.0;
+        int numSteps = 0;
+        
+        while (distanceApart > 0)
+        {
+            distanceApart /= 2;
+            numSteps ++;
+        }
+        
+        System.out.println("The number of steps taken to disprove Zeno was " +
+                numSteps);
+    }
+    
+}
