7f90234db73d1d1e876c56b8ad0818202f4ddb26
Alex Archer <aga7@pitt.edu>
2015-10-04 17:45:50 -0400

    added Lab04b.java

---
+++ Lab04b.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab04b;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alex
+ */
+public class Lab04b {
+
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        byte num = 0;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Level: ");
+        num = sc.nextByte();
+        
+        switch(num)
+        {
+            case 1:
+                System.out.println("Physiological");
+                break;
+                
+            case 2:
+                System.out.println("Physiological \nSafety");
+                
+            case 3:
+                System.out.println("Physiological \nSafety \nLove");
+                
+            case 4:
+                System.out.println("Physiological \nSafety \nLove and Belonging"
+                        + " \nEsteem");
+                
+            case 5:
+                System.out.println("Physiological \nSafety \nLove and Belonging"
+                        + " \nEsteem \nSelf-Actualization");
+                
+                
+               
+        }
+        
+    }
+    
+}
