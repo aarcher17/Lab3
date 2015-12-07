928dd301dfe290ccdc72a6ed89f7c6a208aadadc
Alex Archer <aga7@pitt.edu>
2015-12-06 23:08:23 -0500

    added NoogieException.java

---
+++ NoogieException.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alex
+ */
+package lab13;
+
+public class NoogieException extends Exception{
+    
+    public NoogieException()
+    {
+        super("Error: Noogie Exception");
+    }
+    
+}
